f968e0c488022017186fbbaf004bbc0497123177
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-11 08:15:27 -0500

    added id and rev to properties

--- spatial/all.js
+++ spatial/all.js
@@ -1,6 +1,10 @@
 function(doc){
-	if(doc.geometry && doc.properties){
-		emit(doc.geometry, {geometry:doc.geometry,properties:doc.properties,type: "Feature"});
+	if(doc.geometry && doc.properties && doc._id){
+		var outP = {id:doc._id,rev:doc._rev}
+		for(key in doc.properties){
+			outP[key]=doc.properties[key];
+		}
+		emit(doc.geometry, {"geometry":doc.geometry,"properties":outP});
 	}
 }
 
