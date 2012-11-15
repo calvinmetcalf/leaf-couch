c571b07e6c9acb3f39e8abd5b723ba213f9afc2c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-15 13:37:35 -0500

    works with two indexed dbs

--- _attachments/leaf-couch.js
+++ _attachments/leaf-couch.js
@@ -1,22 +1,23 @@
-var db,rdb, dbPath= document.location.protocol+"//"+document.location.host+"/"+document.location.pathname.split("/")[1];
+var db,rdb, dbPath= "idb://remotecouch";// document.location.protocol+"//"+document.location.host+"/"+document.location.pathname.split("/")[1];
 Pouch("idb://leafcouch",function(e1,db1){
     if(!e1){
         db=db1;
         db.changes({continuous:true,onChange:docChange,include_docs:true},pass);
-        /*Pouch(dbPath,function(e2,db2){
+        Pouch(dbPath,function(e2,db2){
              if(!e2){
                 rdb=db2;
                 db.replicate.to(rdb,{continuous:true});
+                rdb.changes({continuous:true,onChange:rdocChange,include_docs:true},pass);
             }
-        });*/
+        });
     }
 });
 var m= L.map('map').setView([39.40, -96.42], 4),
 	mq=L.tileLayer("http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg", {attribution:'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', subdomains:'1234'}).addTo(m),
 	h = new L.Hash(m),
 	d = new L.Control.Draw().addTo(m),
-	drawnStuff = L.geoJson("",{pointToLayer:pointToLayer,onEachFeature:popUp}).addTo(m);//,
-	//allStuff = L.geoJson.ajax(document.location.protocol+"//"+document.location.host+"/"+document.location.pathname.split("/")[1]+"/"+document.location.pathname.split("/")[2]+"/"+document.location.pathname.split("/")[3]+"/_spatial/_list/geojson/all?bbox="+m.getBounds().toBBoxString(),{pointToLayer:pointToLayer,onEachFeature:popUp});
+	drawnStuff = L.geoJson("",{pointToLayer:pointToLayer,onEachFeature:popUp}).addTo(m),
+	allStuff = L.geoJson("",{pointToLayer:pointToLayer,onEachFeature:popU})
 	m.on('drawn', function (e) {
 			doStuff(e.feature);
 		});
@@ -31,8 +32,8 @@ function redoJson(){
 var baseMaps = {
     "Map Quest": mq
     }, overlayMaps = {
-	"Drawn Layers":drawnStuff//,
-	//"All Layers":allStuff
+	"Drawn Layers":drawnStuff,
+	"All Layers":allStuff
 	},lc=L.control.layers(baseMaps, overlayMaps).addTo(m);
 
 function doStuff(data){
@@ -55,6 +56,15 @@ function popUp(f,l){
         l.bindPopup("<div class='"+ out.lenghth+"' id='" + f._id+"'>"+out.join("<br />")+"</div><br /><input type='button' value='Add Row' id='addRow'><input type='button' value='delete' id='deleteDoc'>");
     }
 }
+function popU(f,l){
+    var out = [];
+    if (f.properties){
+        for(key in f.properties){
+                out.push(key+": "+f.properties[key]);
+        }
+        l.bindPopup(out.join("<br />"));
+    }
+}
 function docChange(c){
 var doc = c.doc;    
 if(doc._rev.slice(0,1)==1){
@@ -66,13 +76,26 @@ drawnStuff.addData(doc);
 }
 }
 }
+function rdocChange(c){
+var doc = c.doc;    
+if(doc._rev.slice(0,1)==1){
+allStuff.addData(doc);
+}else if(doc._rev.slice(0,1)>1){
+    delId(allStuff,doc._id);
+    if(!doc._deleted){
+    allStuff.addData(doc);
+}
+}
+}
 function delId(layer,id){
 layer.eachLayer(function(f){
 if(f.feature._id===id){
 layer.removeLayer(f);
 }
 })}
+var ee;
 m.on("popupopen",function(e){
+    if(e.popup._source._leaflet_id in drawnStuff._layers){
     var id = e.popup._source.feature._id;
     L.DomEvent.addListener(L.DomUtil.get("deleteDoc"),"click",function(click){
         db.get(id, function(err, doc) {
@@ -113,5 +136,5 @@ m.on("popupopen",function(e){
             db.post(dc);
             })
         })
-});
+}});
 function pass(){}
